72d15eb6894936ad342b55b923a7791428bb0ec9
Simon Oulevay <simon.oulevay@gmail.com>
2018-07-10 16:27:53 +0200

    Add password hint in conf/Connection.php

--- conf/Connection.php
+++ conf/Connection.php
@@ -7,14 +7,14 @@
  */
 class Connection {
 
-    private static $CONFIG = "host=localhost port=5432 dbname=DB_NAME user=USERNAME";
+    private static $CONFIG = "host=localhost port=5432 dbname=DB_NAME user=USERNAME password=PASSWORD";
     private $conn;
 
     public function __construct() {
         $this->conn = pg_connect(self::$CONFIG) or die('connection failed');
         pg_set_client_encoding($this->conn, "UTF-8");
     }
-    
+
     public function getConnection(){
         return $this->conn;
     }
